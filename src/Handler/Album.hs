99b1ee62c524d989049ce30a11f9d2ce3d4ada47
dekosuke (Junpei Komiyama) <deco.elb@gmail.com>
2012-03-20 19:45:08 +0900

    アルバムにページング追加

--- src/Handler/Album.hs
+++ src/Handler/Album.hs
@@ -4,16 +4,25 @@ import Import
 import qualified Data.Map as Map
 import Data.Map ((!))
 
---アルバムページ
+
 getAlbumR :: Handler RepHtml
-getAlbumR = do
+getAlbumR = getAlbumPageMainR True 0
+
+--アルバムページ
+getAlbumPageR :: Int -> Handler RepHtml
+getAlbumPageR pageNumber = getAlbumPageMainR False pageNumber
+
+getAlbumPageMainR :: Bool -> Int -> Handler RepHtml
+getAlbumPageMainR isTop pageNumber = do
   maid <- maybeAuthId
   muid <- maybeUserId maid
+  let resultsPerPage = 5
+  let (prevPage, nextPage) = (pageNumber-1, pageNumber+1)
   photoEntities <- runDB $ do
-    selectList [PictureDeleted ==. False] []
+    selectList [PictureDeleted ==. False]
+                [OffsetBy $ pageNumber * resultsPerPage
+                ,LimitTo resultsPerPage]
   let photos = map (\(Entity _ p) -> picturePath p) photoEntities
   defaultLayout $ do
     h2id <- lift newIdent
     $(widgetFile "album")
-
-
