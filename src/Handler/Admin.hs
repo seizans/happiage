0bd83f95ebb749160faa1cc575b3603f655281ff
Shoko Sasaki <sasaki0603shoko@gmail.com>
2012-04-15 11:10:29 +0900

    メールアドレスインポート機能追加

--- src/Handler/Admin.hs
+++ src/Handler/Admin.hs
@@ -2,6 +2,9 @@ module Handler.Admin where
 
 import Import
 
+import qualified Data.Text as DT
+import Data.ByteString.Lazy.Char8 (unpack)
+import Data.List.Split
 -- アップロード用フォーム
 data Email = Email
     {
@@ -11,6 +14,28 @@ data Email = Email
 
 emailForm :: Html -> MForm Happiage Happiage (FormResult Email, Widget )
 emailForm = renderDivs $ Email <$> fileAFormReq "File"
+
+--インポートページ
+getAdminimportR :: Handler RepHtml
+getAdminimportR = do
+    ((_, widget), enctype) <- runFormPost emailForm
+    defaultLayout $ do
+      $(widgetFile "adminimport")
+
+--インポート受け取り
+postAdminimportR :: Handler RepHtml
+postAdminimportR = do
+    ((res, widget), enctype) <- runFormPost emailForm
+    defaultLayout $ case res of
+      FormSuccess email -> do
+        case DT.unpack $ fileContentType $ emailFile email of
+          "text/csv" -> do
+            let mails = splitOn "\n" $ unpack $ fileContent $ emailFile email
+            $(widgetFile "adminimportPost")
+          _ -> do
+            $(widgetFile "adminimport")
+      _ -> do
+        $(widgetFile "adminimport")
 
 --ユーザー管理ページ
 getAdminuserR :: Handler RepHtml
@@ -26,4 +51,4 @@ getAdminuserR = do
         zipJoin [] _ acc = acc
         zipJoin _ [] acc = acc
         zipJoin (x:xs) ys acc = zipJoin xs ys $ (x, (filter (\y -> (userAuthid $ entityVal $ x) == (entityKey $ y)) ys) !! 0) : acc
- 
+
