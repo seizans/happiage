4f98a35cd3ea83334e298f37342ec533e5a24f47
seizans <seizans@gmail.com>
2012-05-12 20:10:30 +0900

    Debug用関数を削除.

--- src/Handler/Root.hs
+++ src/Handler/Root.hs
@@ -1,20 +1,6 @@
 module Handler.Root where
 
 import Import
-import Debug.Trace --printfデバッグ用
-
-traceD :: Show a => a -> b -> b
-traceD a b = trace ("TRACE DEBUG(D):" ++ show a) b
-printD :: Show a => a -> IO ()
-printD a = putStrLn ("TRACE DEBUG(P):" ++ show a)
-
--- This is a handler function for the GET request method on the RootR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 
 --トップページ
 getRootR :: Handler RepHtml
