aeb295f14768611cd05a09f893943eead0e8c3bf
seizans <seizans@gmail.com>
2012-05-13 15:05:44 +0900

    インデント整形

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -6,26 +6,27 @@ import Data.Map ((!))
 
 getMessages :: Handler [(MessageId, Message)]
 getMessages = do
-  messageEntities <- runDB $ selectList [MessageDeleted ==. False] []
-  return $ map (\(Entity pid val) -> (pid, val)) messageEntities
+    messageEntities <- runDB $ selectList [MessageDeleted ==. False] []
+    return $ map (\(Entity pid val) -> (pid, val)) messageEntities
 
 -- メッセージ投稿者、メッセージ本文のリストにする
 composeMessageLine :: (Map.Map UserId User) -> [(MessageId, Message)] -> [(Text, Text)]
 composeMessageLine usersMap [] = []
 composeMessageLine usersMap !messages@((mid, m):ms) =
-  if Map.member (messageUser m) usersMap then
-    (messageUserName, messageText) : composeMessageLine usersMap ms
-  else 
-    composeMessageLine usersMap ms
-  where messageText = messageBody m
-        messageUserName = userName $ usersMap ! (messageUser m)
+    if Map.member (messageUser m) usersMap
+      then
+        (messageUserName, messageText) : composeMessageLine usersMap ms
+      else 
+        composeMessageLine usersMap ms
+  where
+    messageText = messageBody m
+    messageUserName = userName $ usersMap ! (messageUser m)
 
---新郎新婦紹介ページ
+-- 新郎新婦紹介ページ
 getProfileR :: Handler RepHtml
 getProfileR = do
-  usersMap <- getUsersMap
-  messages <- getMessages
-  let messageLines = composeMessageLine usersMap messages
-  defaultLayout $ do
-    h2id <- lift newIdent
-    $(widgetFile "profile")
+    usersMap <- getUsersMap
+    messages <- getMessages
+    let messageLines = composeMessageLine usersMap messages
+    defaultLayout $ do
+        $(widgetFile "profile")
