59a5503691b89844a9f1e3193861df48a0aad489
seizans <seizans@gmail.com>
2012-05-13 22:11:29 +0900

    コード整理

--- src/mkProfile.hs
+++ src/mkProfile.hs
@@ -1,27 +1,18 @@
 {-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses, TemplateHaskell, OverloadedStrings, FlexibleContexts, GADTs #-}
-{-
-import Data.Text (Text)
-import Database.Persist.Quasi
-import Database.Persist.MongoDB
-import Database.Persist.TH
-import Language.Haskell.TH.Syntax
-
-share [mkPersist MkPersistSettings {mpsBackend = ConT '' Action}, mkMigrate "migrateAll"]
-    $(persistFileWith lowerCaseSettings "config/models")
--}
 
 import Model
 import Database.Persist.MongoDB
 
 --main :: IO ()
-main = withMongoDBConn dbname hostname $ runMongoDBConn master $ do
-    insert $ Profile
-      { profileQuestion = "question1"
-      , profileGroom = "groom1"
-      , profileBride = "bride1"
-      , profileOrder = 10
-      }
-    return ()
+main = do
+    withMongoDBConn dbname hostname $ runMongoDBConn master $ do
+        insert $ Profile
+          { profileQuestion = "question1"
+          , profileGroom = "groom1"
+          , profileBride = "bride1"
+          , profileOrder = 10
+          }
+        return ()
   where
     hostname = "localhost"
     dbname = "test"
