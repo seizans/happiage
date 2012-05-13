1782c002f0c74856cb7aec8bac952956ad6538d1
seizans <seizans@gmail.com>
2012-05-13 16:31:46 +0900

    Messageリスト表示をProfilePageからInvitationPageに移動.

--- src/Handler/Invitation.hs
+++ src/Handler/Invitation.hs
@@ -1,10 +1,23 @@
 module Handler.Invitation where
 
 import Import
+import Data.Map (Map, (!))
 
--- 開催案内ページ
+-- InvitationPage(開催案内ページ)
 getInvitationR :: Handler RepHtml
 getInvitationR = do
-    defaultLayout $ do
-        h2id <- lift newIdent
+    msgs <- getMessages
+    usersMap <- getUsersMap
+    let messages = map (mkUsrAndMsg usersMap) msgs
+    defaultLayout $
         $(widgetFile "invitation")
+
+getMessages :: Handler [(MessageId, Message)]
+getMessages = do
+    messageEntities <- runDB $ selectList [MessageDeleted ==. False] []
+    return $ map (\(Entity ident val) -> (ident, val)) messageEntities
+
+mkUsrAndMsg :: Map UserId User -> (MessageId, Message) -> (Text, Text)
+mkUsrAndMsg usersMap (_, message) = (messageBody message, userName user)
+  where
+    user = usersMap ! messageUser message
--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -1,32 +1,10 @@
 module Handler.Profile where
 
 import Import
-import qualified Data.Map as Map
-import Data.Map ((!))
 
-getMessages :: Handler [(MessageId, Message)]
-getMessages = do
-    messageEntities <- runDB $ selectList [MessageDeleted ==. False] []
-    return $ map (\(Entity pid val) -> (pid, val)) messageEntities
-
--- メッセージ投稿者、メッセージ本文のリストにする
-composeMessageLine :: (Map.Map UserId User) -> [(MessageId, Message)] -> [(Text, Text)]
-composeMessageLine usersMap [] = []
-composeMessageLine usersMap !messages@((mid, m):ms) =
-    if Map.member (messageUser m) usersMap
-      then
-        (messageUserName, messageText) : composeMessageLine usersMap ms
-      else 
-        composeMessageLine usersMap ms
-  where
-    messageText = messageBody m
-    messageUserName = userName $ usersMap ! (messageUser m)
-
--- 新郎新婦紹介ページ
+-- ProfilePage(新郎新婦紹介ページ)
 getProfileR :: Handler RepHtml
 getProfileR = do
     usersMap <- getUsersMap
-    messages <- getMessages
-    let messageLines = composeMessageLine usersMap messages
-    defaultLayout $ do
+    defaultLayout $
         $(widgetFile "profile")
